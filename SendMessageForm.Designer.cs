b85995f9b51305cda3267b41f11df76fbc7020b2
RoliSoft <rolisoft@gmail.com>
2016-04-17 18:25:40 +0300

    Added SendMessageForm.

---
+++ SendMessageForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace OctoContacts
+{
+    partial class SendMessageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contactTextBox = new System.Windows.Forms.TextBox();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Contact:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Message:";
+            // 
+            // contactTextBox
+            // 
+            this.contactTextBox.Location = new System.Drawing.Point(65, 12);
+            this.contactTextBox.Name = "contactTextBox";
+            this.contactTextBox.ReadOnly = true;
+            this.contactTextBox.Size = new System.Drawing.Size(406, 20);
+            this.contactTextBox.TabIndex = 3;
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(15, 62);
+            this.messageTextBox.Multiline = true;
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(456, 72);
+            this.messageTextBox.TabIndex = 1;
+            this.messageTextBox.TextChanged += new System.EventHandler(this.messageTextBox_TextChanged);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Enabled = false;
+            this.sendButton.Location = new System.Drawing.Point(396, 140);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 2;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 177);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(488, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(42, 17);
+            this.statusLabel.Text = "Ready.";
+            // 
+            // SendMessageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 199);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.contactTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "SendMessageForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Send SMS";
+            this.Load += new System.EventHandler(this.SendMessageForm_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox contactTextBox;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+    }
+}
---
+++ SendMessageForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+
+namespace OctoContacts
+{
+    public partial class SendMessageForm : Form
+    {
+        public SendMessageForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SendMessageForm_Load(object sender, EventArgs e)
+        {
+            // TODO load contact
+        }
+
+        private void messageTextBox_TextChanged(object sender, EventArgs e)
+        {
+            sendButton.Enabled = messageTextBox.TextLength != 0;
+        }
+
+        private async void sendButton_Click(object sender, EventArgs e)
+        {
+            contactTextBox.Enabled = messageTextBox.Enabled = sendButton.Enabled = false;
+            statusLabel.Text = "Sending message...";
+
+            try
+            {
+                using (var wc = new WebClient())
+                {
+                    await wc.DownloadStringTaskAsync("https://rest.nexmo.com/sms/json?api_key=685ca000&api_secret=7f2833736631bfda&to=40748635048&from=OctoContacts&text=" + Uri.EscapeDataString(messageTextBox.Text));
+                }
+            }
+            catch (Exception)
+            {
+                statusLabel.Text = "Failed.";
+                MessageBox.Show("Failed to send message.", "SMS", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                contactTextBox.Enabled = messageTextBox.Enabled = sendButton.Enabled = true;
+                return;
+            }
+
+            statusLabel.Text = "Done.";
+            MessageBox.Show("Message sent successfully!", "SMS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Close();
+        }
+    }
+}
