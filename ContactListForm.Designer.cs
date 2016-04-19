af9cde0e295c54c54997621774121c9199822892
Bullet <bullett93@gmail.com>
2016-04-19 11:50:15 -0700

    Added ContactListForm

---
+++ ContactListForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace OctoContacts
+{
+    partial class ContactListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView_contacts = new System.Windows.Forms.ListView();
+            this.nameHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.numHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView_contacts
+            // 
+            this.listView_contacts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView_contacts.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nameHeader,
+            this.numHeader});
+            this.listView_contacts.FullRowSelect = true;
+            this.listView_contacts.GridLines = true;
+            this.listView_contacts.Location = new System.Drawing.Point(12, 12);
+            this.listView_contacts.Name = "listView_contacts";
+            this.listView_contacts.Size = new System.Drawing.Size(260, 237);
+            this.listView_contacts.TabIndex = 1;
+            this.listView_contacts.UseCompatibleStateImageBehavior = false;
+            this.listView_contacts.View = System.Windows.Forms.View.Details;
+            // 
+            // nameHeader
+            // 
+            this.nameHeader.Text = "Name";
+            this.nameHeader.Width = 130;
+            // 
+            // numHeader
+            // 
+            this.numHeader.Text = "Number";
+            this.numHeader.Width = 120;
+            // 
+            // ContactListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listView_contacts);
+            this.Name = "ContactListForm";
+            this.Text = "ContactListForm";
+            this.Load += new System.EventHandler(this.ContactListForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView_contacts;
+        private System.Windows.Forms.ColumnHeader nameHeader;
+        private System.Windows.Forms.ColumnHeader numHeader;
+
+
+
+    }
+}
---
+++ ContactListForm.cs
@@ -0,0 +1,42 @@
+﻿using NDatabase;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OctoContacts
+{
+    public partial class ContactListForm : Form
+    {
+        public ContactListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ContactListForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var odb = OdbFactory.Open("octo.db"))
+                {
+                    List<Contact> fromDB = (from cont in odb.AsQueryable<Contact>()
+                                      select cont).ToList();
+                    foreach (var contact in fromDB)
+                    {
+                        var lvi = new ListViewItem(new[] { contact.Name.ToString(), contact.Number });
+                        listView_contacts.Items.Add(lvi);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed to open database.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
